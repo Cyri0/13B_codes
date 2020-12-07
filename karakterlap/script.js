32f9668cafab1cbcc109b7df8c0165980ee5f263
Cyri0 <nemestamas94@gmail.com>
2020-12-07 18:09:52 +0100

    Karakterlap, dec.7 - +komment

--- karakterlap/script.js
+++ karakterlap/script.js
@@ -1,17 +1,30 @@
 class Karakter {
     // OSZTÁLYVÁLTOZÓK
+    /*
+    Ezeket egyébként nem muszáj így leírni, elég lenne csak a constuctor-ban, de mivel
+    a C# ehhez áll közelebb, ezért így írtam.
+    */
     ero = 0;
     egeszseg = 0;
     kitartas = 0;
 
     // KONSTRUKTOR
+    /*
+     Azonnal lefut amint egy példányosítás történik, dolgok előbeállítására szolgál.
+     A this kulcssó az osztály saját változójára mutat (pl.: this.ero).
+    */
     constructor(ero, egeszseg, kitartas){
         this.ero = ero;
         this.egeszseg = egeszseg;
         this.kitartas = kitartas;
     }
 
-    // METÓDUS
+    // METÓDUSOK
+    /* 
+    A viselkedést írják le. 
+    Egy metódus lehet függvény (visszatérési értékkel, pl.: getKitartas()) 
+    vagy eljárás (visszatérési érték nélkül pl.: addEro())
+    */
     addEro(){
         this.ero += 5;
     }
@@ -53,6 +66,11 @@ class Karakter {
 let john = new Karakter(5, 100, 5);
 
 function frissites(){
+    /*
+    Ez az eljárás arra szolgál, hogy ha változás történik az oldalon, 
+    akkor meg kell hívnunk, és megváltoztatja a HTML beli értékeket is.
+    Lásd.: https://www.w3schools.com/js/js_htmldom.asp
+    */
     document.getElementById("ero").innerHTML = john.getEro();
     document.getElementById("kitartas").innerHTML = john.getKitartas();
     document.getElementById("egeszseg").innerHTML = john.getEgeszseg();
